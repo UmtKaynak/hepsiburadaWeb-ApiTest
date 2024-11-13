82e0a96e7d1533fb70c4c120e1af50cb9c49759b
umitkaynak <umit.kaynak@testinium.com>
2024-11-13 14:43:23 +0300

    cookie accept added

--- src/test/java/uiautomation/pages/HomePage.java
+++ src/test/java/uiautomation/pages/HomePage.java
@@ -6,5 +6,7 @@ import org.openqa.selenium.By;
 public class HomePage {
 
     public static final By PRODUCT_COUNT_IN_THE_CART=By.id("cartItemCount");
+    public static final By ACCEPT_COOKIE_BUTTON=By.id("onetrust-accept-btn-handler");
+
 
 }
